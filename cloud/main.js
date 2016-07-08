a151db1908bd209b066c1df2078623dfffe2e42e
Hao Wu <haohao.wu@mail.utoronto.ca>
2016-07-08 16:07:59 -0400

    Fix wrong variable assignment

--- cloud/main.js
+++ cloud/main.js
@@ -4,21 +4,20 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 Parse.Cloud.define("sendRequestToUser", function(request, response) {
-  var message = request.note;
-  var TYPE = request.TYPE;
-  var purpose = request.purpose;
-  var latitude = request.latitude;
-  var longitude = request.longitude;
-  var note = request.note;
-  var rad = request.rad;
-  var category = request.category;
-  var reward = request.reward;
-  var address = request.address;
-  var username = request.username;
-  var rating = request.rating;
+  var TYPE = request.params.TYPE;
+  var purpose = request.params.purpose;
+  var latitude = request.params.latitude;
+  var longitude = request.params.longitude;
+  var note = request.params.note;
+  var rad = request.params.rad;
+  var category = request.params.category;
+  var reward = request.params.reward;
+  var address = request.params.address;
+  var username = request.params.username;
+  var rating = request.params.rating;
   var userpic = "";
-  if(request.hasOwnProperty('userpic')){
-    userpic = request.userpic;
+  if(request.params.hasOwnProperty('userpic')){
+    userpic = request.params.userpic;
   }
   // Send the push.
   // Find devices associated with the recipient user
