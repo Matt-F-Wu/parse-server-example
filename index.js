85486e0f659cd017276097e090988650f8ad6156
Hao Wu <haohao.wu@mail.utoronto.ca>
2016-07-08 17:20:29 -0400

    set up push configurations

--- index.js
+++ index.js
@@ -14,9 +14,15 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'favouramahj',
+  masterKey: process.env.MASTER_KEY || 'haojeremy2016', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://favourama.herokuapp.com/parse',  // Don't forget to change to https if needed
+  push: {
+    android: {
+      senderId: '528033395445', // The Sender ID of GCM
+      apiKey: 'AIzaSyBeWCq0Rb2ipAtjIVe988UkRhbSSAR70p4' // The Server API Key of GCM
+    }
+  }
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
